6e8b3ba7edd1243969fede04ce78aa61ac4b912f
Shafik <abdelrahmanshafik777@gmail.com>
2024-11-18 06:02:40 +0200

    done sign up & sign in

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -8,4 +8,21 @@ window.addEventListener('scroll', function() {
       }
     });
   });
-  
+
+
+
+
+
+
+//for siginin
+
+
+document.getElementById("forgetpass").addEventListener("click", function(event) {
+    event.preventDefault();
+    let userInput = prompt("Please enter your email here:");
+    if (userInput != null) {
+        alert("You password reset link has been sent to: " + userInput);
+    } else {
+        console.log("User cancelled the prompt.");
+    }
+});
